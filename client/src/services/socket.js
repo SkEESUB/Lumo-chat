55af9ccae9567f3b0c9c46a67012d9d2917bf011
Shaik Eesub Vali <shaikeesub376@gmail.com>
2026-04-01 20:53:14 +0530

    added env for backend url

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:5000';
+const URL = import.meta.env.VITE_BACKEND_URL || "https://lumo-backend-9lq7.onrender.com";
 
 export const socket = io(URL, {
   autoConnect: false, // Don't connect until user clicks Join
@@ -13,8 +13,8 @@ export const connectSocket = () => {
 };
 
 export const disconnectSocket = () => {
-    if (socket.connected) {
-        socket.emit('leave_room');
-        socket.disconnect();
-    }
+  if (socket.connected) {
+    socket.emit('leave_room');
+    socket.disconnect();
+  }
 }
